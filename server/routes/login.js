dc9e6b0fd455dfc38885b3d96dd644786c942e5d
Ernesto Angulo <ernestoangulojimenez@gmail.com>
2020-08-10 21:05:28 -0500

    Login post request

--- server/routes/login.js
+++ server/routes/login.js
@@ -5,9 +5,34 @@ const User = require('../models/user');
 const loginRouter = express();
 
 loginRouter.post('/', async (req, res) => {
-  res.json({
-    ok: true,
-  });
+  try {
+    const foundUser = await User.findOne({ email: req.body.email });
+
+    if (!foundUser) {
+      return res.status(400).json({
+        ok: false,
+        message: 'Incorrect (username) or password',
+      });
+    }
+
+    if (!bcript.compareSync(req.body.password, foundUser.password)) {
+      return res.status(400).json({
+        ok: false,
+        message: 'Incorrect username or (password)',
+      });
+    }
+
+    res.json({
+      ok: true,
+      user: foundUser,
+      token: '213',
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      message: error,
+    });
+  }
 });
 
 module.exports = loginRouter;
